d8aac695aae0a463974477ac711b87a062eedcb9
daniel-ayivor <danielkofiayivor23@gmail.com>
2026-09-10 13:27:33 +0000

    Initial commit

--- src/app.ts
+++ src/app.ts
@@ -35,7 +35,7 @@ app.use(cors({
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
 }));
-app.use(express.json());
+
 
 app.use(cookieParser());
 
